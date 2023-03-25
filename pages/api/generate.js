7b3c92dab16a0e2642f2ee9bbc37eddbd194bfc2
AmirSaber Sharifi <inbox@saberistic.com>
2023-03-25 10:50:38 -0400

    a bit of final thoughts

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -39,26 +39,44 @@ export default async function (req, res) {
         content: content,
         name: 'anon',
       }],
-      temperature: 0.6,
+      temperature: 0,
     });
-    console.log(completion.data.choices[0].message.content)
-    const response = JSON.parse(completion.data.choices[0].message.content);
-    if (!response.plan) {
-      console.log(`Denied the following request: ${animal}`);
-      res.status(200).json(completion.data.choices[0].message);
-    } else if (response.plan_options) {
-      console.log(`Suggested the following plans: ${response.plan_options.map(plan => plan.attributes.plan).join(', ')}`);
-      res.status(200).json(completion.data.choices[0].message);
-    } else if (response.plan) {
-      console.log(`Selected the following plan: ${response.plan.attributes.plan}`)
-      const createServerResponse = await sdk.createServer({
-        data: response.plan
-      });
-      console.log(`Created the following server: ${createServerResponse.data.data.attributes.hostname}`);
-      res.status(200).json(response.data.choices[0].message);
+    const message = completion.data.choices[0].message;
+    console.log("Message: ", message);
+    try {
+      const response = JSON.parse(message.content.replace("\n", ""));
+      if (response?.plan) {
+        const plan = response.plan.replaceAll(".", "-");
+        try {
+          const createServerPayload = {
+            data: {
+              type: 'servers',
+              attributes: {
+                project: 'baxus-dev',
+                plan: plan,
+                site: response.region,
+                operating_system: 'ubuntu_20_04_x64_lts',
+                hostname: response.hostname
+              }
+            }
+          };
+          console.log(`Create server payload: ${JSON.stringify(createServerPayload)}`);
+          const createServerResponse = await sdk.createServer(createServerPayload);
+          console.log(`Created the following server: ${createServerResponse.data.data.attributes.hostname}`);
+          return res.status(200).json(message);
+        } catch (error) {
+          console.error(`Error with Latitude API request`, error.data.errors);
+          return res.status(500).json({
+            error: {
+              message: 'Creating Latitude server failed. Contact support.',
+            }
+          });
+        }
+      }
+    } catch (error) {
+      return res.status(200).json(message);
     }
   } catch (error) {
-    console.error(JSON.stringify(error.data));
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -79,36 +97,12 @@ function generatePrompt(request, data) {
 
 ${data.slice(0, 10).map(generatePlans).join('\n')}
 
-Reply to  following input from user in json format ready to be parsed by JSON.parse function:
-input: ${request}
+Reply to  following input from user help user to pick a plan, region and os.: 
+user input: ${request}
 
-if they asked for anything other than creating a virtual machine deny with a creative funny message and tell them what you do in following json format:
-{
-  'role': 'bot',
-  'message': 'produced message'
-}
+reject any other type of request.
 
-if suggesting plans suggest in following json format:
-{
-  'role': 'bot',
-  'plan_options': array of available plans
-}
-
-when ordering virtual machine one of above plans must be specificed
-if they specified plan generate a superhero hostname, fill into below json and reply:
-{
-  'role': 'bot', 
-  'plan': {
-    'type': 'servers',
-    'attributes': {
-      'project': 'baxus-dev',
-      'site': select plan site,
-      'operating_system': 'ubuntu_22_04_x64_lts',
-      'plan': the plan name they selected,
-      'hostname': the superhero hostname
-    }
-  }
-}`;
+when you found a plan, region and os, reply in valid json structure including a generated superhero hostname:`;
 }
 
 function generatePlans(data) {
@@ -134,9 +128,8 @@ function generatePlans(data) {
 
   const availablility = available_in.map(availablity => availablity.sites.map(site => site.in_stock ? site.slug : null).filter(availablity => availablity)).filter(availablity => availablity.length > 0).join(',');
 
-  const description = `Name: ${name}: CPUs - (${cpuDetails}), Memory - ${memory} GB, Drives - (${driveDetails}), Available Sites - (${availablility})`;
+  const description = `Name: ${name}: CPUs - (${cpuDetails}), Memory - ${memory} GB, Drives - (${driveDetails}), Available Regions - (${availablility})`;
   // const description = `Name: ${name}: CPUs - (${cpuDetails}), Memory - ${memory} GB, Drives - (${driveDetails}), NICs - (${nicDetails}), GPUs - (${gpuDetails})`;
-  console.log(description);
 
   return `\n${description}`;
 }
